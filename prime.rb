2b7151063df15244ee5261e6f12d471cd9ed08bf
krae <kraewind@gmail.com>
2020-10-24 14:34:39 -0400

    commit tree 4f9ac38423c48903b4d92fd58ef1ff400cc0bf0f

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+    return false if number.even? && number > 2
+    return false if number <= 0 || number == 1
+    range = (2..number-1).to_a
+    range.each do |integer|
+        return false if (number % integer == 0)
+    end
+    true
+end    
